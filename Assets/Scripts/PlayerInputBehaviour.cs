02ca136c372a275252f835c4ea4cbac9626c5caf
cbs4385 <chris.strube@gmail.com>
2025-10-04 13:19:43 -0400

    Configure PlayerInput with PlayerControls asset

---
+++ Assets/Scripts/PlayerInputBehaviour.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+[DisallowMultipleComponent]
+[AddComponentMenu("Input/Player Input")]
+public sealed class PlayerInputBehaviour : PlayerInput
+{
+}
